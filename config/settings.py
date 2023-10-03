7b60322c1edeec3e64b3d89e5e121918cf7db4e3
Alex_Freud <NikiforenkoAS@i.ua>
2023-10-03 12:20:12 +0300

    Install dotenv. Use dotenv on secret key project

--- config/settings.py
+++ config/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_*p^!#n#*txucfa-m(j*s89pu6!k#^+8u5cmaxwyy2$2iwma4_"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
