44a4312e1e68d2f2fa7dbcc1ad0e1c0c731290d4
Alex_Freud <NikiforenkoAS@i.ua>
2023-10-05 09:55:13 +0300

    Recoding Book model. Added Authot model for books

--- books_service/models.py
+++ books_service/models.py
@@ -1,19 +1,32 @@
 from django.db import models
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    pseudonym = models.CharField(max_length=255, unique=True, blank=True, null=True)
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return f"Author: {self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     class CoverChoices(models.TextChoices):
         HARD = "hard"
         SOFT = "soft"
 
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, blank=True, null=True)
+    author = models.ManyToManyField(to=Author, related_name="books")
     cover = models.CharField(max_length=4, choices=CoverChoices.choices)
-    inventory = models.PositiveIntegerField()  # the number of this specific book available for now in the library
-    daily_fee = models.DecimalField(max_digits=4, decimal_places=2)  # $USD
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=4, decimal_places=2)
 
     def __str__(self):
-        return f"{self.author}, {self.title}"
+        return self.title
 
     class Meta:
         ordering = ["title"]
