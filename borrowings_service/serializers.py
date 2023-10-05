f2e39b1f251c41b06524bc343f40c9e4778b3860
Alex_Freud <NikiforenkoAS@i.ua>
2023-10-05 17:34:34 +0300

    Create BorrowingListSerializer and add perform created in BorrowingViewSet

--- borrowings_service/serializers.py
+++ borrowings_service/serializers.py
@@ -5,16 +5,12 @@ from borrowings_service.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    actual_return_date = serializers.DateField(required=False)
 
     class Meta:
         model = Borrowing
-        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user")
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user", "is_active")
 
 
 class BorrowingListSerializer(BorrowingSerializer):
     book = BookListSerializer(many=False, read_only=True)
-    user = serializers.SlugRelatedField(many=False, read_only=True, slug_field="email")
-
-    class Meta:
-        model = Borrowing
-        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user")
--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -11,17 +11,11 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all().select_related("book", "user")
     serializer_class = BorrowingSerializer
 
-    def get_queryset(self):
-        queryset = self.queryset
-        borrow_date = self.request.query_params.get("borrow_date")
-
-        if borrow_date:
-            queryset = queryset.filter(borrow_date=borrow_date)
-
-        return queryset.distinct()
-
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
             return BorrowingListSerializer
 
         return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
