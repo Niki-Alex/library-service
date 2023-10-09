d30d9b27a2b590f583fc2ac4ba70d8a11ba73baf
Alex_Freud <NikiforenkoAS@i.ua>
2023-10-09 08:50:49 +0300

    Delete user field in Borrowing serializer and add him in BorrowingListSerializer

--- borrowings_service/serializers.py
+++ borrowings_service/serializers.py
@@ -5,12 +5,17 @@ from borrowings_service.models import Borrowing
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    actual_return_date = serializers.DateField(required=False)
+    actual_return_date = serializers.DateField(required=False, read_only=True)
 
     class Meta:
         model = Borrowing
-        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user", "is_active")
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "is_active")
 
 
 class BorrowingListSerializer(BorrowingSerializer):
     book = BookListSerializer(many=False, read_only=True)
+    user = serializers.SlugRelatedField(read_only=True, many=False, slug_field="email")
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user", "is_active")
